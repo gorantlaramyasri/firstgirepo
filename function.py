013b875810ed14e1ca50542adc04f1438338ff50
gorantlaramya <ramya82@gmail.com>
2024-12-27 12:43:50 +0530

    print

---
+++ function.py
@@ -0,0 +1,15 @@
+# Create a function:-
+def greet():
+    print('Good Mornig.!')
+    # Call a function
+greet()
+greet()
+greet()
+greet()
+# 1 predefinde functions:--
+#  len()
+#  type()
+#  int()
+#  str()
+#  print()
+#  list()

5c4ebf0c90f6ac158ddc540d49d8043d234a5aa1
gorantlaramya <ramya82@gmail.com>
2024-12-27 12:39:08 +0530

    print

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-print ('Hello world')
+# print ('Hello world')
 # print('git command')
 # print('git commit')
 
@@ -12,15 +12,22 @@ print ('Hello world')
 # while count <10:
 #     print('Git Hub')    
 
-a = 4
-b = 4
-c = a+b
-print(c)  
+# a = 4
+# b = 4
+# c = a+b
+# print(c)  
 
+def largest_string(*str):
+  list_of_string = []
+  str_len = ""
+  for i in str:
+    if len(i) > len(str_len):
+      str_len = i
+    else:
+      pass
+  print(str_len)  
 
-
-
-
+largest_string("raju", 'Eshu', 'eswar', 'pavankumar', 'krish', 'ravibabu')
 
 
 

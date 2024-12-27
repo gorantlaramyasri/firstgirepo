c71cb2a6e90d144fb4ced6567a3f1e5899350da6
gorantlaramya <ramya82@gmail.com>
2024-12-27 12:28:12 +0530

    print

--- demo.py
+++ demo.py
@@ -1,15 +1,34 @@
 print ('Hello world')
-print('git command')
-print('git commit')
+# print('git command')
+# print('git commit')
 
 
-print('ramya', 'pavani', 'shabista')
+# print('ramya', 'pavani', 'shabista')
 
-str1 = 'enimen 36'
-for i in str1:
-    print(i)
-count = 0
-while count <10:
-    print('Git Hub')    
+# str1 = 'enimen 36'
+# for i in str1:
+#     print(i)
+# count = 0
+# while count <10:
+#     print('Git Hub')    
+
+a = 4
+b = 4
+c = a+b
+print(c)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     

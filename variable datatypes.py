03c792e88b3ecb12aca0dbcd17abf286f9014488
gorantlaramya <ramya82@gmail.com>
2025-02-19 10:28:58 +0530

    variables

---
+++ variable datatypes.py
@@ -0,0 +1,71 @@
+
+# INTIGER
+# a = 2
+# b = 2
+# c = a+b
+# print(c)
+# a = 8
+# b = 6
+# c = a -b
+# print(c)
+# d = a*b
+# print(d)
+# e = a/b
+# print(e)
+
+# f = a//b
+# print(f)
+
+# g = a**b
+# print(g)
+
+# h = a%b
+# print(h)
+
+# FLOAT 
+# a = 3.2
+# b = 4.5
+# c = a+b
+# print(c)
+
+# d = a-b
+# print(d)
+
+# e = a *b
+# print(e)
+
+# f = a/b
+# print(f)
+
+# g = a//b
+# print(g)
+
+# h = a **b
+# print(h)
+
+# i = a%b
+# print(i)
+
+# COMPLEXNUMBER
+
+# a = 34.1
+# b = 2j
+# c = a+b
+# print(c)
+
+# d = a-b
+# print(d)
+
+# e = a*b
+# print(e)
+
+# f = a/b
+# print(f)
+
+# h = a**b
+# print(h)
+
+
+
+
+
